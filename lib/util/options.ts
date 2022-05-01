6ee2887a0f8aa6d53905a7d11976a0415c3d1057
Rida F'kih <hello@rida.dev>
2022-05-01 15:34:40 -0600

    refactor(lib): :coffin: remove unused code

--- lib/util/options.ts
+++ lib/util/options.ts
@@ -1,11 +1,6 @@
 import { PrismaModelField } from "@/modules/PrismaModelField";
 
-import { PrismaFieldTypeName } from "@/@types/prisma-field";
 import { FieldOptions } from "@/@types/prisma-type-options";
-
-export const addOptionsType = 
-	<V extends PrismaFieldTypeName, T>(options: T, type: V) => 
-		({ ...options, type });
 
 export const handleOptions = <T extends FieldOptions>(field: PrismaModelField, options: T) => {	
 	const propertyMap: Record<string, keyof PrismaModelField> = {
