059ac9f1e630cf6d1811eb65bad55d1dcaf7eef9
Roman Rajchert <roman.rajchert@vodafone.com>
2022-11-07 18:04:37 +0100

    feat: prisma enum from JS enum

--- lib/modules/PrismaEnum.ts
+++ lib/modules/PrismaEnum.ts
@@ -1,4 +1,6 @@
-import { PrismaEnumOptions } from "typings/prisma-enum";
+import { PrismaEnumOptions, Enum} from "typings/prisma-enum";
+
+
 
 export class PrismaEnum {
   private enumMap: Map<string, string | undefined> = new Map();
@@ -7,6 +9,17 @@ export class PrismaEnum {
 
   public addValue(value: string, options?: PrismaEnumOptions) {
     this.enumMap.set(value, options?.map);
+    return this;
+  }
+
+  public fromJSEnum<T extends Enum<T>>(
+    enumLike: T,
+    options?: PrismaEnumOptions
+  ) {
+    const values = Object.values(enumLike);
+    values.forEach((value) => {
+      this.enumMap.set(value, options?.map);
+    });
     return this;
   }
 
